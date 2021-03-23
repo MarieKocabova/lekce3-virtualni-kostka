945b5057a93175e5cd6322222d10942dc478cb0e
NTDELLMARUSKA\Marie <maruspondeli@gmail.com>
2021-03-23 20:39:48 +0100

    stav po lekci

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 // tady je místo pro náš program
+
+let textZpravy = document.querySelector("#zprava");
+let obrazek = document.querySelector("#kostka");
+
+
+function hodKostkou() {
+    let hodnotaHodu = Math.floor(Math.random() * 6) +1;
+
+    if (hodnotaHodu === 6) {
+        textZpravy.innerHTML = "Vyhrál jsi!!!";
+    } else {
+        textZpravy.innerHTML = "Hodil jsi " + hodnotaHodu + ". Zkus to znovu."
+    }
+
+    obrazek.src= "obrazky/" + hodnotaHodu + ".png";
+
+}
+
+
+//jednorožec
+
+//top 250px
+//left 450px
+//position absolute
+
+//jednorozec.style.left = 450px;
+//jednorozec.style.top = 250px;
+
+//button onclick="venaDoOhrady"
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+
+//jak najít pozicování prvku 
